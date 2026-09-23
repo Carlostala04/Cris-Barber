e28aded1e442b62b7f6d2afbb835b7139f054037
Carlostala04 <carlostala.dev@gmail.com>
2026-09-23 11:50:12 -0600

    se agrego el componente TeamCard

--- cris-barber-frontend/src/pages/Home.tsx
+++ cris-barber-frontend/src/pages/Home.tsx
@@ -1,10 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { imagesHero } from "../assets/data/images";
-import "../styles/home.css"
+import "../styles/home.css";
+import imagen from "../assets/imagenPrueba.png"
+import TeamCard from "../components/teamCard";
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isFading, setIsFading] = useState(false);
-
+  const teamMembers = [
+    {
+      nombre: "Carlos Rodríguez",
+      descripcion: "Especialista en diseño y desarrollo web",
+      ocupacion: "Desarrollador Frontend",
+      imagen: imagen,
+    },
+    {
+      nombre: "María González",
+      descripcion: "Experta en estrategias digitales y marketing",
+      ocupacion: "Marketing Manager",
+      imagen: imagen,
+    },
+    {
+      nombre: "Andrés Vargas",
+      descripcion: "Encargado de crear experiencias visuales atractivas",
+      ocupacion: "Diseñador UI/UX",
+      imagen: imagen,
+    },
+    {
+      nombre: "Sofía Martínez",
+      descripcion: "Especialista en gestión y coordinación de proyectos",
+      ocupacion: "Project Manager",
+      imagen: imagen,
+    },
+    {
+      nombre: "Daniel Fernández",
+      descripcion: "Desarrollador especializado en servidores y APIs",
+      ocupacion: "Backend Developer",
+      imagen: imagen,
+    },
+  ];
   useEffect(() => {
     const intervalMs = 3000;
 
@@ -32,6 +65,30 @@ export default function Home() {
         <div className="hero-actions">
           <button>Agendar cita</button>
           <button>Contactanos</button>
+        </div>
+      </section>
+      <section className="team-section">
+        <header className="team-section-header">
+          <h3>
+            <span className="line"></span>Equipo
+          </h3>
+          <div className="main-title">
+            <h1>Conoce a nuestro equipo</h1>
+            <p>
+              Manos habilidosas, ojos agudos y años de experiencia detras de la
+              silla
+            </p>
+          </div>
+        </header>
+        <div className="team-card">
+          {teamMembers.map((team) => (
+            <TeamCard
+              nombre={team.nombre}
+              descripcion={team.descripcion}
+              ocupacion={team.ocupacion}
+              imagen={team.imagen}
+            />
+          ))}
         </div>
       </section>
     </div>
