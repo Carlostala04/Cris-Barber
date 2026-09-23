7130a72267cd3d9e685ba32c4f1f96b4903d694b
Carlostala04 <carlostala.dev@gmail.com>
2026-09-23 14:48:19 -0600

    se agrego el componente header

--- cris-barber-frontend/src/App.tsx
+++ cris-barber-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
