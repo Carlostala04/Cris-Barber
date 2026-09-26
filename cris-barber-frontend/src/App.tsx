493c0236998fd913f9566d86f502dd71424e019b
Carlostala04 <carlostala.dev@gmail.com>
2026-09-25 19:37:41 -0600

    se agrego la ruta de la pagina de citas y se elimino el header

--- cris-barber-frontend/src/App.tsx
+++ cris-barber-frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Header from "./components/Header";
+import Appointment from "./pages/Appointment";
 
 function App() {
   return (
     <>
-    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cita" element={<Appointment/>}/>
       </Routes>
     </>
   );
