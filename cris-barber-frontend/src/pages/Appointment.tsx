5e8603d38ee16e4aa7d2ee2f6ac0db7190da0644
Carlostala04 <carlostala.dev@gmail.com>
2026-09-25 19:48:06 -0600

    se creo el archivo de la pagina de cita

---
+++ cris-barber-frontend/src/pages/Appointment.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Leave from "../assets/icons/arrow";
+
+export default function Appointment() {
+  return (
+    <div className="appointment">
+      <a href="">
+        Volver <Leave />
+      </a>
+    </div>
+  );
+}
